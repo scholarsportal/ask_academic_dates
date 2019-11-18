038b1add081beff2b200b5633e26803a914b6333
Guinsly Mondesir <guinslym@gmail.com>
2019-11-18 18:52:15 -0500

    adding pytest

--- tests/test_ask_academic_dates.py
+++ tests/test_ask_academic_dates.py
@@ -1,5 +1,11 @@
 from ask_academic_dates import __version__
-
+from ask_academic_dates import find_academic_year
+from datetime import date
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_check_find_academic_year():
+    given_date = date(2019,5,3)
+    result = find_academic_year(date(2019,5,3))
+    assert result == '2018-2019'
